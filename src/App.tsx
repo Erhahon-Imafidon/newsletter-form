7c50b418fc283ca129e0fc424b3fd9a5ec7b77b1
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-04 08:22:02 +0100

    Installed prettier and run the prettier to format my code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 const App = () => {
-  return (
-    <div className='flex justify-center items-center'>
-        <h1 className='text-amber-400'>App</h1>
-    </div>
-  )
-}
+    return (
+        <div className="flex justify-center items-center">
+            <h1 className="text-amber-400">App</h1>
+        </div>
+    );
+};
 
-export default App
+export default App;
